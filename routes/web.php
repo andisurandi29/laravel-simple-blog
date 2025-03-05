6693b935792720badf03a82c1da3aef7acd8f52f
andisurandi29 <andisurandi2908@gmail.com>
2025-03-05 15:56:19 +0700

    Feat : Route Optimization

--- routes/web.php
+++ routes/web.php
@@ -7,26 +7,19 @@ use App\Http\Controllers\ProfileController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-// Routes untuk post yang butuh autentikasi
-Route::controller(PostController::class)->group(function () {
-    Route::middleware('auth')->group(function () {
-        Route::get('/posts/create', 'create')->name('posts.create');
-        Route::post('/posts', 'store')->name('posts.store');
-        Route::get('/posts/{post}/edit', 'edit')->name('posts.edit');
-        Route::put('/posts/{post}', 'update')->name('posts.update');
-        Route::delete('/posts/{post}', 'destroy')->name('posts.destroy');
-    });
-    Route::get('/posts', 'index')->name('posts.index');
-    Route::get('/posts/{post}', 'show')->whereNumber('post')->name('posts.show');
-});
-
-
 Route::middleware('auth')->group(function () {
+    Route::resource('posts', PostController::class)->except(['index', 'show']);
+
     Route::controller(ProfileController::class)->group(function () {
         Route::get('/profile', 'edit')->name('profile.edit');
         Route::patch('/profile', 'update')->name('profile.update');
         Route::delete('/profile', 'destroy')->name('profile.destroy');
     });
+});
+
+Route::controller(PostController::class)->group(function () {
+    Route::get('/posts', 'index')->name('posts.index');
+    Route::get('/posts/{post}', 'show')->name('posts.show');
 });
 
 require __DIR__.'/auth.php';
